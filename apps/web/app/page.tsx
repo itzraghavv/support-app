89a55d063afbc981a6172c4e09ca9eaa3aeeb7af
raghav-97 <raghavtandulkar97@gmail.com>
2025-08-07 23:27:07 +0530

    added convex package/backend

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,14 @@
-import { Button } from "@workspace/ui/components/button"
+"use client";
+
+import { api } from "@workspace/backend/_generated/api";
+import { useQuery } from "convex/react";
 
 export default function Page() {
+  const users = useQuery(api.user.getUser);
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
-      </div>
+    <div>
+      web/page.tsx
+      {JSON.stringify(users)}
     </div>
-  )
+  );
 }
