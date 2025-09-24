2fdd45ede26bd4103bcdfab16e967c60225af44b
raghav-97 <raghavtandulkar97@gmail.com>
2025-09-24 17:41:17 +0530

    vapi-integration

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,15 +1,30 @@
 "use client";
 
+import { useVapi } from "@/modules/widget/hooks/use-vapi";
 import { api } from "@workspace/backend/_generated/api";
+import { Button } from "@workspace/ui/components/button";
 import { useQuery } from "convex/react";
 
 export default function Page() {
-  const users = useQuery(api.user.getUser);
+  const {
+    isConnected,
+    isConnecting,
+    isSpeaking,
+    transcript,
+    startCall,
+    endCall,
+  } = useVapi();
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      web/page.tsx
-      <div className="max-w-sm w-full mx-auto">{JSON.stringify(users)}</div>
+    <div className="flex flex-col items-center justify-center min-h-svh max-w-md mx-auto w-full">
+      <Button onClick={() => startCall()}>Start Call</Button>
+      <Button variant="destructive" onClick={() => endCall()}>
+        End Call
+      </Button>
+      <p>isConnected : {`${isConnected}`}</p>
+      <p>isSpeaking : {`${isSpeaking}`}</p>
+      <p>isConnecting : {`${isConnecting}`}</p>
+      <p>{JSON.stringify(transcript, null, 2)}</p>
     </div>
   );
 }
